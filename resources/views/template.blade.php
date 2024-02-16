72c78178bf010367d33c293d815f700caecd3729
maulanasolehudn <solehudinmaulana75@gmail.com>
2024-02-16 16:53:41 +0700

    ujikom

---
+++ resources/views/template.blade.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>uplod image</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
